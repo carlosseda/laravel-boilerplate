24264ef57247c09c2db352cf9eca31eb585afe4f
carlosseda <carlossedagambin@gmail.com>
2023-12-04 16:45:12 +0100

    Filter component

--- resources/views/components/admin-table-filter.blade.php
+++ resources/views/components/admin-table-filter.blade.php
@@ -1,6 +1,5 @@
 @props([
   'filters' => [],
-  'tabs' => false,
 ])
 
 <section class="filter">
@@ -11,95 +10,81 @@
 
     <div class="filter-form">
       <form class="table-filter">
-        @if ($tabs)
-          <div class="tabs-content">
-            @foreach ($inputs as $key => $fields)
-              <div class="tab-content {{ $loop->first ? 'active' : '' }}" data-tab="{{ $key }}">
-                <div class="form-elements">
-                  @foreach ($fields as $field)
-                    <div class="form-element {{$field['width']}}">
-                      <div class="form-element-label">
-                        <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
-                      </div>
-                      <div class="form-element-input">
-                        @if ($field['type'] === 'textarea')
-                          <textarea name="{{ $field['name'] }}" id="{{ $field['name'] }}"></textarea>
-                        @elseif ($field['type'] === 'file')
-                          <input type="file" name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                        @elseif ($field['type'] === 'select')
-                          <select name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                            @foreach ($field['options'] as $option)
-                              <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
-                            @endforeach
-                          </select>
-                        @else
-                          @if ($field['type'] === 'checkbox')
-                            @foreach ($field['options'] as $option)
-                              <div class="form-element-checkbox">
-                                <input type="checkbox" name="{{ $field['name'] }}[]" id="{{ $field['name'] }}-{{ $option['value'] }}" value="{{ $option['value'] }}">
-                                <label for="{{ $field['name'] }}-{{ $option['value'] }}">{{ $option['label'] }}</label>
-                              </div>
-                            @endforeach
-                          @elseif ($field['type'] === 'radio')
-                            @foreach ($field['options'] as $option)
-                              <div class="form-element-radio">
-                                <input type="radio" name="{{ $field['name'] }}" id="{{ $field['name'] }}-{{ $option['value'] }}" value="{{ $option['value'] }}">
-                                <label for="{{ $field['name'] }}-{{ $option['value'] }}">{{ $option['label'] }}</label>
-                              </div>
-                            @endforeach
-                          @else
-                            <input type="{{ $field['type'] }}" name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                          @endif
-                        @endif
-                      </div>
-                    </div>
-                  @endforeach
-                </div>
-              </div>
-            @endforeach
-          </div>
-        @else
-          <div class="form-elements">
-            @foreach ($filters as $field)
-              <div class="form-element {{$field['width']}}">
-                <div class="form-element-label">
-                  <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
-                </div>
-                <div class="form-element-input">
-                  @if ($field['type'] === 'textarea')
-                    <textarea name="{{ $field['name'] }}" id="{{ $field['name'] }}"></textarea>
-                  @elseif ($field['type'] === 'file')
-                    <input type="file" name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                  @elseif ($field['type'] === 'select')
-                    <select name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                      @foreach ($field['options'] as $option)
-                        <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
-                      @endforeach
-                    </select>
-                  @else
-                    @if ($field['type'] === 'checkbox')
-                      @foreach ($field['options'] as $option)
-                        <div class="form-element-checkbox">
-                          <input type="checkbox" name="{{ $field['name'] }}[]" id="{{ $field['name'] }}-{{ $option['value'] }}" value="{{ $option['value'] }}">
-                          <label for="{{ $field['name'] }}-{{ $option['value'] }}">{{ $option['label'] }}</label>
-                        </div>
-                      @endforeach
-                    @elseif ($field['type'] === 'radio')
-                      @foreach ($field['options'] as $option)
-                        <div class="form-element-radio">
-                          <input type="radio" name="{{ $field['name'] }}" id="{{ $field['name'] }}-{{ $option['value'] }}" value="{{ $option['value'] }}">
-                          <label for="{{ $field['name'] }}-{{ $option['value'] }}">{{ $option['label'] }}</label>
-                        </div>
-                      @endforeach
-                    @else
-                      <input type="{{ $field['type'] }}" name="{{ $field['name'] }}" id="{{ $field['name'] }}">
-                    @endif
-                  @endif
-                </div>
-              </div>
-            @endforeach
-          </div>
-        @endif
+        <div class="tabs-content">
+          @foreach ($filters as $formElement)
+            @switch($formElement['type'])
+              @case('textarea')
+                <x-form.textarea 
+                  :name="$formElement['name']"
+                  :width="$formElement['width']" 
+                  :label="$formElement['label']" 
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+
+              @case('image')
+                <x-form.file
+                  :name="$formElement['name']"
+                  :width="$formElement['width']" 
+                  :label="$formElement['label']" 
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+      
+              @case('file')
+                <x-form.file
+                  :name="$formElement['name']"
+                  :width="$formElement['width']" 
+                  :label="$formElement['label']" 
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+      
+              @case('select')
+
+                <x-form.select
+                  :name="$formElement['name']"
+                  :width="$formElement['width']" 
+                  :label="$formElement['label']" 
+                  :options="$formElement['options']"
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+      
+              @case('checkbox')
+
+                <x-form.checkbox
+                  :name="$formElement['name']"
+                  :width="$formElement['width']"
+                  :label="$formElement['label']"
+                  :options="$formElement['options']"
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+      
+              @case('radio')
+
+                <x-form.radio
+                  :name="$formElement['name']"
+                  :width="$formElement['width']"
+                  :label="$formElement['label']"
+                  :options="$formElement['options']"
+                  :inputAttributes="$formElement['attributes'] ?? ''"
+                />
+                @break
+      
+              @default
+                <x-form.input
+                  :name="$formElement['name']"
+                  :width="$formElement['width']"
+                  :label="$formElement['label']"
+                  :type="$formElement['type']"
+                  :inputAttributes="$formElement['attributes'] ?? []"
+                />
+                @break
+            @endswitch
+          @endforeach
+        </div>
       </form>
     </div>
 
